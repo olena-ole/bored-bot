3d46c3dc91882606f2b7bff1e5f17fcec0cfa9af
Lenna <pampam32121@gmail.com>
2022-06-11 04:25:28 +0300

    added event listener for button, changed styling

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const getBtn = document.querySelector('#get-btn');
+
+function getNewIdea() {
+    
+    fetch('https://apis.scrimba.com/bored/api/activity')
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#activity-name').textContent = data.activity;
+            document.getElementById('title').textContent = '🥳 HappyBot 🤩';
+        });
+};
+
+getBtn.addEventListener('click', getNewIdea);
